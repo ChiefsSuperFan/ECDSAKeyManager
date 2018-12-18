c7489724208d41b31549ca44633dfc7e22db24b1
Anthony Stone <Anthony.Stone@formativelogic.com>
2018-12-18 15:15:01 -0500

    project setup

--- ECDSAKeyManager/Form1.Designer.cs
+++ ECDSAKeyManager/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ECDSAKeyManager
 {
-    partial class Form1
+    partial class frmAdmin
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnCreateUser = new System.Windows.Forms.Button();
+            this.txtPrivateKey = new System.Windows.Forms.TextBox();
+            this.txtPublicKey = new System.Windows.Forms.TextBox();
+            this.txtUserAddress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.txtFileURL = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtHash = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMAC = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnCreateUser
+            // 
+            this.btnCreateUser.Location = new System.Drawing.Point(77, 285);
+            this.btnCreateUser.Name = "btnCreateUser";
+            this.btnCreateUser.Size = new System.Drawing.Size(199, 66);
+            this.btnCreateUser.TabIndex = 0;
+            this.btnCreateUser.Text = "Create User";
+            this.btnCreateUser.UseVisualStyleBackColor = true;
+            // 
+            // txtPrivateKey
+            // 
+            this.txtPrivateKey.Location = new System.Drawing.Point(251, 71);
+            this.txtPrivateKey.Name = "txtPrivateKey";
+            this.txtPrivateKey.Size = new System.Drawing.Size(1186, 38);
+            this.txtPrivateKey.TabIndex = 1;
+            // 
+            // txtPublicKey
+            // 
+            this.txtPublicKey.Location = new System.Drawing.Point(251, 139);
+            this.txtPublicKey.Name = "txtPublicKey";
+            this.txtPublicKey.Size = new System.Drawing.Size(1199, 38);
+            this.txtPublicKey.TabIndex = 2;
+            // 
+            // txtUserAddress
+            // 
+            this.txtUserAddress.Location = new System.Drawing.Point(264, 208);
+            this.txtUserAddress.Name = "txtUserAddress";
+            this.txtUserAddress.Size = new System.Drawing.Size(1173, 38);
+            this.txtUserAddress.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 32);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Private Key:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 32);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Public Key:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(213, 32);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Public Address:";
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(1192, 458);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(115, 43);
+            this.btnSelect.TabIndex = 7;
+            this.btnSelect.Text = "button1";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            // 
+            // txtFileURL
+            // 
+            this.txtFileURL.Location = new System.Drawing.Point(236, 458);
+            this.txtFileURL.Name = "txtFileURL";
+            this.txtFileURL.Size = new System.Drawing.Size(934, 38);
+            this.txtFileURL.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(45, 458);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 32);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "File URL:";
+            // 
+            // txtHash
+            // 
+            this.txtHash.Location = new System.Drawing.Point(236, 539);
+            this.txtHash.Name = "txtHash";
+            this.txtHash.Size = new System.Drawing.Size(1122, 38);
+            this.txtHash.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(45, 539);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(143, 32);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "File Hash:";
+            // 
+            // txtMAC
+            // 
+            this.txtMAC.Location = new System.Drawing.Point(236, 638);
+            this.txtMAC.Name = "txtMAC";
+            this.txtMAC.Size = new System.Drawing.Size(1122, 38);
+            this.txtMAC.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(67, 638);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 32);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "MAC:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // frmAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1797, 873);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtMAC);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtHash);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtFileURL);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUserAddress);
+            this.Controls.Add(this.txtPublicKey);
+            this.Controls.Add(this.txtPrivateKey);
+            this.Controls.Add(this.btnCreateUser);
+            this.Name = "frmAdmin";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCreateUser;
+        private System.Windows.Forms.TextBox txtPrivateKey;
+        private System.Windows.Forms.TextBox txtPublicKey;
+        private System.Windows.Forms.TextBox txtUserAddress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.TextBox txtFileURL;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtHash;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMAC;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
---
+++ ECDSAKeyManager/SupportingClasses.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace ECDSAKeyManager
+{
+    public class UserAccount
+    {
+        private const int ADDRESS_LENGTH = 40;
+
+        public string ECDSAPrivateKey { get; private set; }
+        public string ECSDAPublicKey { get; private set; }
+        public string UserAddress { get; private set; }
+
+        public UserAccount()
+        {
+
+            CngKeyCreationParameters keyCreationParameters = new CngKeyCreationParameters();
+            keyCreationParameters.ExportPolicy = CngExportPolicies.AllowPlaintextExport;
+            keyCreationParameters.KeyUsage = CngKeyUsages.Signing;
+
+            CngKey key = CngKey.Create(CngAlgorithm.ECDsaP256, null, keyCreationParameters);
+
+
+
+            ECDsaCng dsa = new ECDsaCng(key); //dsa = Digital Signature Algorithm
+            byte[] privateKey = dsa.Key.Export(CngKeyBlobFormat.EccPrivateBlob);
+
+            ECDSAPrivateKey = BitConverter.ToString(privateKey).Replace("-", String.Empty);
+
+
+
+            //create public key
+            byte[] publicKey = dsa.Key.Export(CngKeyBlobFormat.EccPublicBlob);
+
+            ECSDAPublicKey = BitConverter.ToString(publicKey).Replace("-", String.Empty);
+
+
+
+            //we calculate the hashvalue by slicing the end off of the public key
+            string hashValue = ECDSAOperations.GetHashValue(publicKey);
+
+            int hashLength = hashValue.Length;
+            UserAddress = "0x" + hashValue.Substring(hashLength - ADDRESS_LENGTH);
+
+        }
+    }
+
+    public static class ECDSAOperations
+    {
+        public static string GetHashValue(byte[] InputValue)
+        {
+            SHA256Managed sha = new SHA256Managed();
+            byte[] checksum = sha.ComputeHash(InputValue);
+
+
+
+            return BitConverter.ToString(checksum).Replace("-", String.Empty);
+
+
+
+        }
+
+        public static string GetSha256FileHash(string FileURL)
+        {
+
+            try
+            {
+                using (FileStream stream = File.OpenRead(FileURL))
+                {
+                    SHA256Managed sha = new SHA256Managed();
+                    byte[] checksum = sha.ComputeHash(stream);
+
+
+
+
+                    return BitConverter.ToString(checksum).Replace("-", String.Empty);
+                }
+
+
+
+            }
+            catch
+            {
+
+                return "";
+            }
+        }
+
+        public static bool VerifySignature(string Message, string Signature, string PublicKey)
+        {
+
+            try
+            {
+                byte[] publicKey = ConvertToBytes(PublicKey);
+                byte[] message = ConvertToBytes(Message);
+                byte[] signature = ConvertToBytes(Signature);
+
+                bool verified = false;
+                using (CngKey importedKey = CngKey.Import(publicKey, CngKeyBlobFormat.EccPublicBlob))
+                {
+                    using (ECDsaCng importedDSA = new ECDsaCng(importedKey))
+                    {
+                        verified = importedDSA.VerifyData(message, signature);
+                    }
+                }
+
+
+                return verified;
+
+
+
+            }
+            catch
+            {
+                return false;
+            }
+
+
+        }
+
+        private static byte[] ConvertToBytes(string InputValue)
+        {
+            //we are often converting byte arrays to strings
+            //this ACCURATELY converts them back!
+
+            int arrayLength = InputValue.Length / 2;
+            //the inputvalues will always be twice the length of the
+            //byte array because when you use bit converter, each byte
+            //is converted to a 2 character hex number
+            byte[] convert = new byte[arrayLength];
+            int index = -1;
+
+            try
+            {
+
+
+                char[] inputParts = InputValue.ToCharArray();
+                string hex = "";
+                foreach (char part in inputParts)
+                {
+
+
+                    hex += part.ToString();
+                    if (hex.Length == 2)
+                    {
+                        index++;
+                        convert[index] = Convert.ToByte(hex, 16);
+
+                        hex = "";
+                    }
+                }
+                return convert;
+
+            }
+            catch
+            {
+                return convert;
+            }
+        }
+
+    }
+}
